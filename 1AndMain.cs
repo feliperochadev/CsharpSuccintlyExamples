8970a94a18ea0284216df59817683c8d882c41a6
Felipe Rocha <feliperocha.ti@gmail.com>
2016-07-27 23:36:02 -0300

    code listing 4 to 10

--- 1AndMain.cs
+++ 1AndMain.cs
@@ -8,10 +8,30 @@ class Program
         //Console.Write("Greetings");       
         
         //2 and 3
-        double hypotenuse = Calc.Pythagorean(2, 3);
-        Console.WriteLine($"Hypotenuse: {hypotenuse}");
-        //Crypto.AesManaged aes = new Crypto.AesManaged();
-        Console.ReadKey();
+        // double hypotenuse = Calc.Pythagorean(2, 3);
+        // Console.WriteLine($"Hypotenuse: {hypotenuse}");
+
+        //4
+        //int count = 7;
+        //char keyPressed = 'Q';
+        //string title = "Weekly Report";
+        
+        //5
+        //string title;
+        //6
+        //title = "Weekly Report";
+
+
+        //7 and 8
+        int total = int.Parse("359");
+        string message = 7.ToString();
+
+        //9
+        double preciseLength = 5.61;
+        int roundedLength = (int)preciseLength;
+
+        //10
+        decimal price = 9.95m;
 
     }
 }
