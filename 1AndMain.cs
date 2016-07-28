e9f73645bbcc0ef213bd6c54aa132e9c112e751c
Felipe Rocha <feliperocha.ti@gmail.com>
2016-07-27 23:58:00 -0300

    Code listing 20 to 25

--- 1AndMain.cs
+++ 1AndMain.cs
@@ -46,7 +46,7 @@ class Program
         // int val4 = val3--;
 
         // //14
-        // decimal priceGain = 2.5m;
+         decimal priceGain = 2.5m;
         // string action = priceGain > 2m ? "Buy" : "Sell";
 
         // //15
@@ -56,14 +56,61 @@ class Program
         // string defaultDateString = currentTime.ToString();
         // DateTime Tomorrow = currentTime.AddDays(1);
 
-        //16
-        int result1 = 2 + 3 * 5;
-        int result2 = (2 + 3) * 5;
+        // //16
+        // int result1 = 2 + 3 * 5;
+        // int result2 = (2 + 3) * 5;
 
-        //17
-        string name = "Joe";
-        string helloViaConcatenation = "Hello, " + name + "!";
-        Console.WriteLine(helloViaConcatenation);
+        // //17
+        // string name = "Joe";
+        // string helloViaConcatenation = "Hello, " + name + "!";
+        // Console.WriteLine(helloViaConcatenation);
+
+        // //18 and 19
+        // string helloviaStringFormat = string.Format("Hello, {0}!", name);
+        // Console.WriteLine(helloviaStringFormat);
+
+        //20
+        string item = "bread";
+        decimal amount = 2.25m;
+        Console.WriteLine("{0, -10}{1:C}", item, amount);
+
+        //21
+        Console.WriteLine($"{item}   {amount}");
+
+        //22
+        Console.WriteLine($"{nameof(item)}: {item,-10} {nameof(amount)}: {amount:C}");
+
+        //23
+        string action2 = "Sell";
+        if(priceGain > 2m)
+        {
+            action2 = "Buy";
+        }
+
+        //24
+        string action3 = "Do Nothing";
+        if(priceGain <= 2m)
+        {
+            action3 = "Sell";
+        }
+        else
+        {
+            action3 = "Buy";
+        }
+
+        //25
+        string action4 = null;
+        if(priceGain <= 2m)
+        {
+            action4 = "Sell";
+        }else if (priceGain > 2m && priceGain <= 3m)
+        {
+            action4 = "Do Nothing";
+        }
+        else
+        {
+            action4 = "Sell";
+        }
 
     }
 }
